8867a4eab56c29862995ccb1082dd78a48b67eb9
Ratomir Vukadin <ratomir@live.com>
2024-01-05 09:20:46 +0100

    dotnet ef migrations

--- development/docker-build.sh
+++ development/docker-build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Define a new parameter for the tag
+tag=$1
+
+if [ -z "$tag" ]; then
+    tag="latest"
+fi
+
+echo "Tag for this build is -> $tag"
+
 echo "Starting Docker build process for resttest..."
 docker build -t resttest -f src/Web/Dockerfile src/
 echo "Docker build completed successfully."
+
+# Re-tagging the image
+docker tag resttest $tag
---
+++ development/ef-migrations-update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Path to the install.sh script
+INSTALL_SCRIPT_PATH="./development/install.sh"
+
+# Check if dotnet is installed
+if ! command -v dotnet &> /dev/null; then
+    echo "dotnet is not installed. Running install.sh to install .NET Core SDK."
+    if [ -f "$INSTALL_SCRIPT_PATH" ]; then
+        bash "$INSTALL_SCRIPT_PATH"
+        if [ $? -ne 0 ]; then
+            echo "Failed to install .NET Core SDK."
+            exit 1
+        fi
+    else
+        echo "install.sh script not found at $INSTALL_SCRIPT_PATH."
+        exit 1
+    fi
+fi
+
+# Check if dotnet-ef tool is installed
+if ! command -v dotnet ef --help &> /dev/null; then
+    echo "dotnet ef is not installed. Installing now..."
+    dotnet tool install --global dotnet-ef
+    if [ $? -ne 0 ]; then
+        echo "Failed to install dotnet ef."
+        exit 1
+    fi
+fi
+
+echo "dotnet ef is installed."
+export DOTNET_ROOT=/usr/share/dotnet
+
+# Check if the first argument is empty
+if [ -z "$1" ]; then
+    echo "Error: Connection string is required as the first argument."
+    exit 1  # Exit with status 1 to indicate an error
+fi
+
+connectionString=$1
+
+echo "Default Connection String: $connectionString"
+
+# Execute the dotnet command
+dotnet ef database update --project ./src/Infrastructure/Infrastructure.csproj --startup-project ./src/Web/Web.csproj --connection "$connectionString"
+
+echo ////////////////////////
+echo     Migrations done
+echo ////////////////////////
